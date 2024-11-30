91d9cb535df91c5e50e3fab6608be559c9b8218c
Avinash77H <arakholiya5@gmail.com>
2024-11-30 05:58:02 +0530

    sales_order_details table foreign key does not exist in product_master table

--- Notes.sql
+++ Notes.sql
@@ -370,6 +370,15 @@ create table salesman_master(
 */
 
 /*
+insert into salesman_master
+values
+('S00001','Aman','A/14','Worli','Mumbai',400002,'Maharashtra',3000,100,50,'Good'),
+('S00002','Omkar','65','Nariman','Mumbai',400001,'Maharashtra',3000,200,100,'Good'),
+('S00003','Raj','P-7','Bandra','Mumbai',400032,'Maharashtra',3000,200,100,'Good'),
+('S00004','Ashish','A/5','Juhu','Mumbai',400044,'Maharashtra',3500,200,150,'Good');
+*/
+
+/*
 create table sales_order(
     ORDERNO varchar(6) primary key,
     CLIENTNO varchar(6),
@@ -386,6 +395,17 @@ create table sales_order(
 */
 
 /*
+insert into sales_order
+values
+('O19001','C00001','2004-06-12',null,'S00001','F','N','2002-07-20','In Process'),
+('O19002','C00002','2004-06-25',null,'S00002','P','N','2002-07-27','Cancelled'),
+('O46865','C00003','2004-02-18',null,'S00003','F','Y','2002-02-20','Fulfilled'),
+('O19003','C00001','2004-04-03',null,'S00001','F','Y','2002-04-07','Fulfilled'),
+('O46866','C00004','2004-05-20',null,'S00002','P','N','2002-05-22','Cancelled'),
+('O19008','C00005','2004-05-24',null,'S00004','F','N','2002-07-26','In Process');
+*/
+
+/*
 create table sales_order_details(
     ORDERNO varchar(6),
     PRODUCTNO varchar(6),
@@ -396,3 +416,20 @@ create table sales_order_details(
     foreign key (PRODUCTNO) references product_master (PRODUCTNO)
 );
 */
+
+insert into sales_order_details
+values
+('O19001','P00001',4,4,525),
+('O19001','P07956',2,1,8400),
+('O19001','P07885',2,1,5250),
+('O19002','P00001',10,0,525),
+('O46865','P07868',3,3,3150),
+('O46865','P07885',3,1,5250),
+('O46865','P00001',10,10,525),
+('O46865','P00345',4,4,1050),
+('O19003','P03453',2,2,1050),
+('O19003','P06734',1,1,12000),
+('O46866','P07965',1,0,8400),
+('O46866','P07975',1,0,1050),
+('O19008','P00001',10,5,525),
+('O19008','P07975',5,3,1050);
