f3030b1ae5429cb237cdf6c0b99c1019a2bcfc7f
vijay dankhara <vijaydankhara111@gmail.com>
2025-01-21 15:24:12 +0530

    routing completed

--- Node/01-Express Server Creation-js/server.js
+++ Node/01-Express Server Creation-js/server.js
@@ -9,9 +9,9 @@ app.get('/',(request,response)=>{
     response.status(200);
     response.json({
         msg:"Hello I am Avi..."
-    })
-})
+    });
+});
 
-app.listen(port,hostName , () => {
-    console.log(`Server is Started at http://${hostName}:${port}`)
-})
+app.listen(port,hostName,() => {
+    console.log(`Server is Started at http://${hostName}:${port}`);
+});
---
+++ Node/02-baseRouting/server.ts
@@ -0,0 +1,20 @@
+import express,{Application,Request,Response} from 'express'
+import userRouter from './Routes/userRouter';
+
+const hostName:string = '127.0.0.1';
+const port:number = 9999;
+
+const app:Application = express();
+
+app.get("/",(request:Request,response:Response)=>{
+    response.status(200);
+    response.json({
+        msg:"Hello world"     
+    });
+});
+
+app.use('/api/users',userRouter)
+
+app.listen(port,hostName,()=>{
+    console.log(`express server run on http://${hostName}:${port}`)
+})
