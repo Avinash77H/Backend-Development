1845056b71bce3f49a3e88e5016b8f5b33070944
Avinash77H <arakholiya5@gmail.com>
2025-02-03 10:44:28 +0530

    ...

--- Node/skillQode/04-Express-json-without-mvc/router/userRouter.ts
+++ Node/skillQode/04-Express-json-without-mvc/router/userRouter.ts
@@ -4,14 +4,12 @@ const jsonfile = require('jsonfile')
 const userRouter:Router = Router();
 import {IUser} from "../models/IUser"
 
-
 userRouter.get("/",(request:Request,response:Response)=>{
   const usersJsonPath = path.join(__dirname,"..","db","users.json")
   jsonfile.readFile(usersJsonPath,  (err:string, obj:IUser) =>{
     if (err) console.error(err)
     response.json(obj);
   })
-   
 });
 
 export default userRouter;  
